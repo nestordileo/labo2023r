2dc9644d429077535f6340ee451bd85894b1dd1e
Néstor Di Leo <127801559+nestordileo@users.noreply.github.com>
2023-07-17 18:54:53 -0300

    Flow polska
    Update and rename impar701_RUN_correr_workflow.r to polska701_RUN_correr_workflow.r

---
+++ src/workflow-semillerio/polska701_RUN_correr_workflow.r
@@ -0,0 +1,16 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow semillerio
+source("~/labo2023r/src/workflow-semillerio/polska711_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-semillerio/polska721_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-semillerio/polska731_FE_exp2.r")
+source("~/labo2023r/src/workflow-semillerio/polska741_TS_under03.r")
+source("~/labo2023r/src/workflow-semillerio/polska751_HT_regul_cv10.r")
+source("~/labo2023r/src/workflow-semillerio/polska771_ZZ_final_semillerio.r")
