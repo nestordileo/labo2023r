8fc5d1597bc2ad432ff302e23ec2ba168392a7f8
nestordileo <nestor.dileo2009@gmail.com>
2023-07-18 12:02:49 +0000

    701 exp 4

---
+++ src/workflow-semillerio/impar701_RUN_exp4.r
@@ -0,0 +1,16 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow semillerio
+source("~/labo2023r/src/workflow-semillerio/impar711_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-semillerio/impar721_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-semillerio/impar731_FE_exp4.r")
+source("~/labo2023r/src/workflow-semillerio/impar741_TS_under05.r")
+source("~/labo2023r/src/workflow-semillerio/impar751_HT_regul_cv10.r")
+source("~/labo2023r/src/workflow-semillerio/impar771_ZZ_exp4.r")
