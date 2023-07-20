ed8c1f76e6a3516ca70ba1bede7999e0ad52d8c5
Néstor Di Leo <127801559+nestordileo@users.noreply.github.com>
2023-07-19 23:49:45 -0300

    Update and rename polska701_RUN_exp10.r to polska701_RUN_exp12.r

--- src/workflow-semillerio/polska701_RUN_exp12.r
+++ src/workflow-semillerio/polska701_RUN_exp12.r
@@ -10,7 +10,7 @@ options(error = function() {
 # corrida de cada paso del workflow semillerio
 source("~/labo2023r/src/workflow-semillerio/polska711_CA_reparar_dataset.r")
 source("~/labo2023r/src/workflow-semillerio/polska721_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-semillerio/polska731_FE_exp10.r")
-source("~/labo2023r/src/workflow-semillerio/polska741_TS_under03.r")
+source("~/labo2023r/src/workflow-semillerio/polska731_FE_exp12.r")
+source("~/labo2023r/src/workflow-semillerio/polska741_TS_under05.r")
 source("~/labo2023r/src/workflow-semillerio/polska751_HT_regul_cv10.r")
-source("~/labo2023r/src/workflow-semillerio/polska771_ZZ_exp10.r")
+source("~/labo2023r/src/workflow-semillerio/polska771_ZZ_exp12.r")
