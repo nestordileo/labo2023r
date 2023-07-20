f8c374bcc1174218420b1af2f8d321c960dd6226
Néstor Di Leo <127801559+nestordileo@users.noreply.github.com>
2023-07-20 07:26:18 -0300

    Update and rename impar701_RUN_exp9.r to impar701_RUN_exp15.r

--- src/workflow-semillerio/impar701_RUN_exp15.r
+++ src/workflow-semillerio/impar701_RUN_exp15.r
@@ -10,7 +10,7 @@ options(error = function() {
 # corrida de cada paso del workflow semillerio
 source("~/labo2023r/src/workflow-semillerio/impar711_CA_reparar_dataset.r")
 source("~/labo2023r/src/workflow-semillerio/impar721_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-semillerio/impar731_FE_exp9.r")
-source("~/labo2023r/src/workflow-semillerio/impar741_TS_under03.r")
+source("~/labo2023r/src/workflow-semillerio/impar731_FE_exp15.r")
+source("~/labo2023r/src/workflow-semillerio/impar741_TS_under05.r")
 source("~/labo2023r/src/workflow-semillerio/impar751_HT_regul_cv5.r")
-source("~/labo2023r/src/workflow-semillerio/impar771_ZZ_exp9.r")
+source("~/labo2023r/src/workflow-semillerio/impar771_ZZ_exp15.r")
