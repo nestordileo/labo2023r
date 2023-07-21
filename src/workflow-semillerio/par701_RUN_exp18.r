5d11dd68647869a6d336742fbb4b7fa433068a4d
Néstor Di Leo <127801559+nestordileo@users.noreply.github.com>
2023-07-21 12:44:37 -0300

    Update and rename par701_RUN_exp16.r to par701_RUN_exp18.r

---
+++ src/workflow-semillerio/par701_RUN_exp18.r
@@ -0,0 +1,16 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow semillerio
+source("~/labo2023r/src/workflow-semillerio/par711_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-semillerio/par721_DR_deflacion.r")
+source("~/labo2023r/src/workflow-semillerio/par731_FE_exp18.r")
+source("~/labo2023r/src/workflow-semillerio/par741_TS_pan2_under05.r")
+source("~/labo2023r/src/workflow-semillerio/par751_HT_sinregul_cv5.r")
+source("~/labo2023r/src/workflow-semillerio/par771_ZZ_exp18.r")
