3ae49be6cbc3fbceae82bc68059672bff30fb9e2
Néstor Di Leo <127801559+nestordileo@users.noreply.github.com>
2023-07-21 13:05:18 -0300

    Update par701_RUN_exp18.r
    con under 03 agregad

--- src/workflow-semillerio/par701_RUN_exp18.r
+++ src/workflow-semillerio/par701_RUN_exp18.r
@@ -11,6 +11,6 @@ options(error = function() {
 source("~/labo2023r/src/workflow-semillerio/par711_CA_reparar_dataset.r")
 source("~/labo2023r/src/workflow-semillerio/par721_DR_deflacion.r")
 source("~/labo2023r/src/workflow-semillerio/par731_FE_exp18.r")
-source("~/labo2023r/src/workflow-semillerio/par741_TS_pan2_under05.r")
+source("~/labo2023r/src/workflow-semillerio/par741_TS_pan2_under03.r")
 source("~/labo2023r/src/workflow-semillerio/par751_HT_sinregul_cv5.r")
 source("~/labo2023r/src/workflow-semillerio/par771_ZZ_exp18.r")
