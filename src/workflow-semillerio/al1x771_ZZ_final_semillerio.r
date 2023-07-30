de790cc3c944544afc0a638d7fd017a7868316e2
Néstor Di Leo <127801559+nestordileo@users.noreply.github.com>
2023-07-30 06:45:18 -0300

    Update al1x771_ZZ_final_semillerio.r
    15 sem

--- src/workflow-semillerio/al1x771_ZZ_final_semillerio.r
+++ src/workflow-semillerio/al1x771_ZZ_final_semillerio.r
@@ -30,7 +30,7 @@ PARAM$modelos_rank <- c(1)
 # cantidad de semillas a utilizar en el semillerio,
 #  podrian ser 50 o 100 para mayor estabilidad
 #  semillerio <- 1  es el ZZ_final  tradicional
-PARAM$semillerio <- 20
+PARAM$semillerio <- 15
 
 # se utiliza para generar el vector de  PARAM$semillerio  semillas
 PARAM$semilla_primos <- 999983
