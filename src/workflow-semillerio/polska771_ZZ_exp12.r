b861022d6cbc7a7a1de4d6b1f1e4a416cbd1de79
Néstor Di Leo <127801559+nestordileo@users.noreply.github.com>
2023-07-31 01:00:21 -0300

    Update and rename polska771_ZZ_exp19.r to polska771_ZZ_exp12.r

--- src/workflow-semillerio/polska771_ZZ_exp12.r
+++ src/workflow-semillerio/polska771_ZZ_exp12.r
@@ -20,8 +20,8 @@ require("lightgbm")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "ZZ7710polska190"
-PARAM$exp_input <- "HT7510polska19"
+PARAM$experimento <- "ZZ7710polska120"
+PARAM$exp_input <- "HT7510polska12"
 
 # Que modelos quiero, segun su posicion en el ranking
 # de la Bayesian Optimizacion, ordenado por ganancia descendente
