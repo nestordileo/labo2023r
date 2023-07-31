c645c6e4abc2e0b8d430a75b1e43ac283b8be904
Néstor Di Leo <127801559+nestordileo@users.noreply.github.com>
2023-07-31 00:35:01 -0300

    Update and rename polska721_DR_deflacion.r to polska721_DR_exp19.r

--- src/workflow-semillerio/polska721_DR_exp19.r
+++ src/workflow-semillerio/polska721_DR_exp19.r
@@ -11,9 +11,9 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "DR7210polska"
+PARAM$experimento <- "DR7210polska19"
 
-PARAM$exp_input <- "CA7110polska"
+PARAM$exp_input <- "CA7110polska19"
 
 PARAM$variables_intrames <- TRUE # atencion esto esta en TRUE
 
